b30143f2a6d031f99fe4e1cf59c36c91a29d6208
Codemac <codemac@Codemacs-MacBook-Pro.local>
2018-02-27 17:52:56 +0530

    first commit

---
+++ CoreDataTutorial/nextViewController.swift
@@ -0,0 +1,35 @@
+//
+//  nextViewController.swift
+//  CoreDataTutorial
+//
+//  Created by Codemac on 27/02/2018.
+//  Copyright © 2018 Codemac. All rights reserved.
+//
+
+import UIKit
+
+class nextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
